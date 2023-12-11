a72d32a39f5b04dd2fb7b525c3d3978bb418d8bc
sebasgiraldo1123 <sebasgiraldo1123@gmail.com>
2023-12-11 10:07:23 -0500

    Actualización

--- behaviors/hillClimbing.py
+++ behaviors/hillClimbing.py
@@ -1,42 +1,42 @@
 class HillClimbing:
     def __init__(self, robot):
+        # Inicializar con una instancia del robot.
         self.robot = robot
 
     def search(self) -> list:
-        # Obtener la posición inicial del robot
+        # Obtener la grilla del modelo y la posición inicial del robot.
+        grid = self.robot.model.grid
         current_x, current_y = self.robot.pos
-        current_cost = self.robot.get_heuristic(current_x, current_y)
+
+        # Lista para almacenar los pasos realizados durante la búsqueda.
         steps = [(current_x, current_y)]
+        # Conjunto para registrar las celdas visitadas.
+        visited = set([(current_x, current_y)])
 
         while True:
-            neighbors = self.get_neighbors(current_x, current_y)
-            next_node = None
-            next_cost = current_cost
+            best_heuristic = float('inf')
+            next_step = None
 
-            # Explorar los vecinos para encontrar una mejor solución
-            for (nx, ny) in neighbors:
-                cost = self.robot.get_heuristic(nx, ny)
-                if cost < next_cost:
-                    next_node = (nx, ny)
-                    next_cost = cost
+            # Explorar los vecinos del nodo actual.
+            for dx, dy in self.robot.directions:
+                new_x, new_y = current_x + dx, current_y + dy
+                # Verificar si la nueva celda es transitable y no ha sido visitada.
+                cellmates = grid.get_cell_list_contents([(new_x, new_y)])
+                if (new_x, new_y) not in visited and self.robot.verifyWay(cellmates):
+                    # Calcular la heurística para el vecino.
+                    heuristic = self.robot.get_heuristic(new_x, new_y)
+                    # Seleccionar el vecino con la mejor heurística.
+                    if heuristic < best_heuristic:
+                        best_heuristic = heuristic
+                        next_step = (new_x, new_y)
 
-            # Si no se encuentra un mejor vecino, termina la búsqueda
-            if next_node is None:
+            # Si no se encuentra un mejor vecino, terminar la búsqueda.
+            if next_step is None:
                 break
 
-            # Moverse al mejor vecino encontrado
-            current_x, current_y = next_node
-            current_cost = next_cost
-            steps.append(next_node)
+            # Moverse al mejor vecino encontrado y actualizar la posición actual.
+            current_x, current_y = next_step
+            steps.append(next_step)
+            visited.add(next_step)  # Añadir el nuevo nodo a los visitados.
 
         return steps
-
-    def get_neighbors(self, x, y):
-        # Generar y devolver los vecinos de la posición actual
-        grid = self.robot.model.grid
-        neighbors = []
-        for dx, dy in self.robot.directions:
-            cellmates = grid.get_cell_list_contents([(x + dx, y + dy)])
-            if self.robot.verifyWay(cellmates) or self.robot.verifyflag(cellmates):
-                neighbors.append((x + dx, y + dy))
-        return neighbors
